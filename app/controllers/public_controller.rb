7022ac5fe6e20c27fd0e9fc0d9259ed9a8aac7c0
Scott Johnson <7.scott.j@gmail.com>
2011-08-17 16:51:50 -0500

    update sms response with some known values

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -6,13 +6,16 @@ class PublicController < ApplicationController
   
   # should respond to post
   def sms
-    ps = params
-    ps.delete("controller")
-    ps.delete("action")
-    unless ps.empty?
-      sms_back = TwilioNet.new().send_sms('2146680255', ps)
+    account_sid = params[:AccountSid]
+    body = params[:body]
+    from = params[:from]
+    from_zip =  params[:FromZip]
+    if true #account_sid == TWILIO_ACCOUNT_SID && from && body
+      sms_back = TwilioNet.new().send_sms('2146680255', truncate("FROM:#{from}  BODY:#{body}", 150) )
     end 
     render :text => "OK" 
+  rescue
+    render :text => "Invalid Params"
   end
   
 end
