571666558e63d9fa8218b0382168abb5e68e6919
Scott Johnson <7.scott.j@gmail.com>
2011-08-17 17:05:00 -0500

    better txt response

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,21 +1,28 @@
 class PublicController < ApplicationController
+  # MixIn ActionView::Helpers::TextHelper for truncate 
+  include ActionView::Helpers::TextHelper
   # before_filter :authenticate
   
   def index
   end
   
+  def test_truncate
+    truncate("my string", :length => 6)
+  end
+  
   # should respond to post
   def sms
-    account_sid = params[:AccountSid]
+    # account_sid = params[:AccountSid]
     body = params[:body]
     from = params[:from]
-    from_zip =  params[:FromZip]
-    if true #account_sid == TWILIO_ACCOUNT_SID && from && body
-      sms_back = TwilioNet.new().send_sms('2146680255', truncate("FROM:#{from}  BODY:#{body}", 150) )
+    # from_zip =  params[:FromZip]
+    if false #account_sid == TWILIO_ACCOUNT_SID && from && body
+      sms = TwilioNet.new()
+      sms.send_sms('2146680255', truncate("FROM:#{from}  BODY:#{body}", :length => 159) )
     end 
     render :text => "OK" 
   rescue
-    render :text => "Invalid Params"
+    render :text => "Invalid Params #{params.inspect}"
   end
   
 end
