a1dbb862eae8b890c01326e8754dc2a724311e3c
Scott Johnson <7.scott.j@gmail.com>
2011-08-16 22:27:25 -0500

    fix public url for production

--- config/initializers/box.rb
+++ config/initializers/box.rb
@@ -4,7 +4,7 @@ BOX_API_KEY =apiconfig["box"]["api_key"]  #put your box api key here
 BOX_API_URL ="https://www.box.net/api/1.0"  #box api location
 
 BASE_URI = if ENV['RAILS_ENV'] == "production"
-   "http://open.ftp.com"
+   "http://open-ftp.com"
 else
    "http://localhost:3000"
 end    
