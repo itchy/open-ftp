3d511a7aacabcc685abd55234f5571935d5b3aea
Scott Johnson <7.scott.j@gmail.com>
2011-08-24 02:01:30 -0500

    tweaks to heroku evn variables

--- config/initializers/box.rb
+++ config/initializers/box.rb
@@ -1,10 +1,17 @@
-apiconfig = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/api.yml.private')
-
-BOX_API_KEY = ENV['BOX_API_KEY'] || apiconfig["box"]["api_key"]  #put your box api key here
-BOX_API_URL ="https://www.box.net/api/1.0"  #box api location
+if ENV['RAILS_ENV'] == "production"
+   yml_file = '/api.yml'
+else
+   yml_file = '/api.yml.private'
+end
 
 BASE_URI = if ENV['RAILS_ENV'] == "production"
    "http://open-ftp.com"
 else
    "http://localhost:3000"
-end    
+end
+
+apiconfig = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + yml_file)
+
+BOX_API_KEY = ENV['BOX_API_KEY'] || apiconfig["box"]["api_key"]  #put your box api key here
+BOX_API_URL ="https://www.box.net/api/1.0"  #box api location
+
--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,3 +1,10 @@
+if ENV['RAILS_ENV'] == "production"
+   yml_file = '/api.yml'
+else
+   yml_file = '/api.yml.private'
+end
+
+
 apiconfig = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/api.yml.private')
 
 TWILIO_ACCOUNT_SID= ENV['TWILIO_ACCOUNT_SID'] || apiconfig["twilio"]["account_sid"]  
