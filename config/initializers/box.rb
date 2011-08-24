eecf0874eb8f2038e3b7aa8648f986803313dbcd
Scott Johnson <7.scott.j@gmail.com>
2011-08-24 01:55:47 -0500

    Use heroku ENV variables for API configs

--- config/initializers/box.rb
+++ config/initializers/box.rb
@@ -1,6 +1,6 @@
-apiconfig = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/api.yml')
+apiconfig = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/api.yml.private')
 
-BOX_API_KEY =apiconfig["box"]["api_key"]  #put your box api key here
+BOX_API_KEY = ENV['BOX_API_KEY'] || apiconfig["box"]["api_key"]  #put your box api key here
 BOX_API_URL ="https://www.box.net/api/1.0"  #box api location
 
 BASE_URI = if ENV['RAILS_ENV'] == "production"
--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,5 @@
-apiconfig = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/api.yml')
+apiconfig = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/api.yml.private')
 
-TWILIO_ACCOUNT_SID=apiconfig["twilio"]["account_sid"]  
-TWILIO_AUTH_TOKEN=apiconfig["twilio"]["auth_token"]  
-TWILIO_NUMBER=apiconfig["twilio"]["phone_number"].to_s
+TWILIO_ACCOUNT_SID= ENV['TWILIO_ACCOUNT_SID'] || apiconfig["twilio"]["account_sid"]  
+TWILIO_AUTH_TOKEN= ENV['TWILIO_AUTH_TOKEN'] || apiconfig["twilio"]["auth_token"]  
+TWILIO_NUMBER= ENV['TWILIO_NUMBER'] || apiconfig["twilio"]["phone_number"].to_s
